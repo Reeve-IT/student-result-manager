4a10a735bb09fb044db9e5d22ec091369ff23431
Reeve-IT <dabreoreeve89@gmail.com>
2026-02-22 23:28:15 +0530

    added improved version code

---
+++ featured(advanced code).py
@@ -0,0 +1,83 @@
+students =[]
+
+#basic work flow
+while True:
+    print("\n1. Add Student")
+    print("2. View Student")
+    print("3. Exit")
+    
+    choice=input("Enter your choice:")
+    
+    if choice=="1":
+        print("Add Student selected")
+        
+        #stdent data
+        name=input("Enter the name of the student:")
+        s1=int(input("Enter Math marks:"))
+        s2=int(input("Enter Java marks:"))
+        s3=int(input("Enter Python marks:"))
+
+        total=s1+s2+s3
+        if total >300:
+            print("Error!! check subject marks entered :(")
+        else:
+            print("Total Marks=",total,"/300")
+            
+             #percentage calculation
+             # percentage=total/3
+             # print(percentage)
+            maxixum_marks=300
+            percentage=(total/maxixum_marks)*100
+            print("Percentage=",percentage,"%")
+            
+            #grade assigning
+            if percentage >=75:
+                 grade="A"
+                        
+            elif percentage >=60:
+                grade="B"
+            elif percentage >=40:
+                grade="C"
+            else:
+                grade="Fail"
+            print("Grade",grade)
+            
+            #store student data
+            student={"name":name,
+                     "marks":[s1,s2,s3],
+                     "percentage":percentage,
+                     "grade":grade}
+            
+            students.append(student)
+            print("Student data added successfully")
+            print(students)
+           
+    elif choice=="2":
+        print("view Student selected")
+        
+        if len(students)==0:
+            print("no student data found")
+            
+        else:
+            print("\nStudent list")
+            for i, student in enumerate(students,start=1):
+                print(i,"-",student["name"])
+                
+            choice=int(input("select student number:"))
+            if choice < 1 or choice > len(students):
+                print("Invalid syntax")
+            else:
+                student=students[choice -1]
+            
+            print("\nStudent details")
+            print("Name",student["name"])
+            print("Marks",student,["marks"])
+            print("Percentage:",student["percentage"])
+            print("Grade:",student["grade"])
+            print("-----------------")
+             
+    elif choice=="3":
+        break
+    else:
+        print("Invalid choice")
+        
